4beff1468416b570ecac701aa9aacd18eed79df3
Ishu Bansal <ishubansal1400@gmail.com>
2025-12-29 12:19:11 +0530

    hydrate perio records on load

--- src/app/store/ReduxProvider.tsx
+++ src/app/store/ReduxProvider.tsx
@@ -5,13 +5,17 @@ import { store } from './store';
 import { ReactNode, useEffect } from 'react';
 import { setPatients } from './patientSlice';
 import { loadStateFromLocalStorage } from './localStorage';
+import { setPerioRecords } from './perioSlice';
 
 export default function ReduxProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     // Load state from localStorage when the app initializes
     const savedState = loadStateFromLocalStorage();
-    if (savedState && savedState.patients && savedState.patients.patients) {
+    if (savedState?.patients?.patients) {
       store.dispatch(setPatients(savedState.patients.patients));
+    }
+    if (savedState?.perio?.records) {
+      store.dispatch(setPerioRecords(savedState.perio.records));
     }
   }, []);
 
