b39cfe7220ce7dda9884e312a86ebae8d85afbbc
Ishu Bansal <ishubansal1400@gmail.com>
2025-12-30 13:42:14 +0530

    add todos

--- src/app/perio/constants.ts
+++ src/app/perio/constants.ts
@@ -6,3 +6,14 @@ export const ZONES = [
   { label: "2", size: 3 },
   { label: "3", size: 3 }
 ];
+
+/**
+ * TODO:
+ * - [ ] Add notes for each record
+ * - [ ] Introduce custom bulk operations in Datatable
+ * - [ ] Implement copy to csv for records
+ * - [ ] Handle negative values in perio input
+ * - [ ] Select missing teeth on record creation
+ * - [ ] Generate quick input rows dynamically based on missing/selected teeth info
+ * - [ ] Define a standard for perio charting
+ */
