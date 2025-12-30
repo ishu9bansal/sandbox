d51a60941ffdfc63c4cb4b327b64ce16df8e6c0c
Ishu Bansal <ishubansal1400@gmail.com>
2025-12-30 19:15:51 +0530

    inject mapping dependency

--- src/models/theeth.ts
+++ src/models/theeth.ts
@@ -4,13 +4,16 @@ type FixedArray<T, N extends number, A extends T[] = []> =
 
 type Quadrant<T> = FixedArray<FixedArray<T, 8>, 4>; // 4 quadrants, each with 8 teeth
 
+const COMMON_TOOTH_MAPPING = [
+  [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28],
+  [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38],
+];
+
 class TeethGrid<DataType> {
   quadrants: Quadrant<DataType>;
-  private mapping = [
-    [18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28],
-    [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38],
-  ];
-  constructor(data: Quadrant<DataType>) {
+  private mapping: number[][];
+  constructor(data: Quadrant<DataType>, mapping: number[][] = COMMON_TOOTH_MAPPING) {
+    this.mapping = mapping;
     this.quadrants = data;
   }
   getData(): DataType[][] {
