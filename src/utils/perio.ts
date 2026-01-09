297b78da47355d560ba46e6a8d4815f972731010
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-09 17:33:50 +0530

    move input mapping to separate file

--- src/utils/perio.ts
+++ src/utils/perio.ts
@@ -2,6 +2,7 @@ import { PerioRecord } from "@/models/perio";
 import { MappingType, Serializer } from "@/models/perioInput";
 import { generateDefaultTeeth } from "@/models/theeth";
 import { ModelInput } from "@/models/type";
+import { INPUT_MAPPING } from "./perioMappings";
 
 // Helper function to generate unique record IDs
 export const generateRecordId = (): string => {
@@ -18,85 +19,6 @@ export function generateNewRecord(): ModelInput<PerioRecord> {
     patientId: null,
   };
 }
-
-export const INPUT_MAPPING: MappingType = [
-  [
-    '18BMe', '18BMi', '18BDi',
-    '17BMe', '17BMi', '17BDi',
-    '16BMe', '16BMi', '16BDi',
-    '15BMe', '15BMi', '15BDi',
-    '14BMe', '14BMi', '14BDi',
-    '13BMe', '13BMi', '13BDi',
-    '12BMe', '12BMi', '12BDi',
-    '11BMe', '11BMi', '11BDi',
-
-    '21BMe', '21BMi', '21BDi',
-    '22BMe', '22BMi', '22BDi',
-    '23BMe', '23BMi', '23BDi',
-    '24BMe', '24BMi', '24BDi',
-    '25BMe', '25BMi', '25BDi',
-    '26BMe', '26BMi', '26BDi',
-    '27BMe', '27BMi', '27BDi',
-    '28BMe', '28BMi', '28BDi',
-  ],
-  [
-    '18LMe', '18LMi', '18LDi',
-    '17LMe', '17LMi', '17LDi',
-    '16LMe', '16LMi', '16LDi',
-    '15LMe', '15LMi', '15LDi',
-    '14LMe', '14LMi', '14LDi',
-    '13LMe', '13LMi', '13LDi',
-    '12LMe', '12LMi', '12LDi',
-    '11LMe', '11LMi', '11LDi',
-
-    '21LMe', '21LMi', '21LDi',
-    '22LMe', '22LMi', '22LDi',
-    '23LMe', '23LMi', '23LDi',
-    '24LMe', '24LMi', '24LDi',
-    '25LMe', '25LMi', '25LDi',
-    '26LMe', '26LMi', '26LDi',
-    '27LMe', '27LMi', '27LDi',
-    '28LMe', '28LMi', '28LDi',
-  ],
-  [
-    '48LMe', '48LMi', '48LDi',
-    '47LMe', '47LMi', '47LDi',
-    '46LMe', '46LMi', '46LDi',
-    '45LMe', '45LMi', '45LDi',
-    '44LMe', '44LMi', '44LDi',
-    '43LMe', '43LMi', '43LDi',
-    '42LMe', '42LMi', '42LDi',
-    '41LMe', '41LMi', '41LDi',
-
-    '31LMe', '31LMi', '31LDi',
-    '32LMe', '32LMi', '32LDi',
-    '33LMe', '33LMi', '33LDi',
-    '34LMe', '34LMi', '34LDi',
-    '35LMe', '35LMi', '35LDi',
-    '36LMe', '36LMi', '36LDi',
-    '37LMe', '37LMi', '37LDi',
-    '38LMe', '38LMi', '38LDi',
-  ],
-  [
-    '48BMe', '48BMi', '48BDi',
-    '47BMe', '47BMi', '47BDi',
-    '46BMe', '46BMi', '46BDi',
-    '45BMe', '45BMi', '45BDi',
-    '44BMe', '44BMi', '44BDi',
-    '43BMe', '43BMi', '43BDi',
-    '42BMe', '42BMi', '42BDi',
-    '41BMe', '41BMi', '41BDi',
-
-    '31BMe', '31BMi', '31BDi',
-    '32BMe', '32BMi', '32BDi',
-    '33BMe', '33BMi', '33BDi',
-    '34BMe', '34BMi', '34BDi',
-    '35BMe', '35BMi', '35BDi',
-    '36BMe', '36BMi', '36BDi',
-    '37BMe', '37BMi', '37BDi',
-    '38BMe', '38BMi', '38BDi',
-  ],
-];
 
 export const REPORT_MAPPING: MappingType = [
   INPUT_MAPPING.flat()
