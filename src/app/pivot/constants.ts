5f402f61ba09fb0f7945ca83f5655cd431a626bf
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-11 18:33:42 +0530

    modify placeholder text

--- src/app/pivot/constants.ts
+++ src/app/pivot/constants.ts
@@ -1,2 +1,6 @@
-export const PLACEHOLDER_INPUT = "Paste your pivot table headers here...";
+export const PLACEHOLDER_INPUT = `Paste your pivot table headers here...
+G1\t\t\t\t\tG2\t\t\t\t\t
+H1\t\tH2\t\t\tH1\t\tH2\t\t
+A\tB\tC\tA\tB\tC\tA\tB\tC\tA\tB\tC
+`;
 export const PLACEHOLDER_OUTPUT = "Processed output will appear here.";
