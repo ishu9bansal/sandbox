35dabca5b51e1a00f3d7064a116331313b41c8a8
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-14 14:03:35 +0530

    remove simulation code for now

--- src/app/ticker/components/InstrumentsView.tsx
+++ src/app/ticker/components/InstrumentsView.tsx
@@ -2,7 +2,7 @@
 import DataTable from '@/components/DataTable';
 import { columnsBuilder } from '@/components/DataTable/columns';
 import { Column } from '@/components/DataTable/types';
-import { useTickerFetch } from '@/hooks/useTickerFetch';
+import { useInstruments } from '@/hooks/useTickerFetch';
 import { Instrument } from '@/models/ticker';
 import { useAppSelector } from '@/store/hooks';
 import { selectInstruments } from '@/store/slices/tickerSlice';
@@ -10,7 +10,7 @@ import { useMemo } from 'react';
 
 export default function InstrumentView() {
   const instruments = useAppSelector(selectInstruments);
-  const fetchInstruments = useTickerFetch();
+  const reload = useInstruments();
 
   const columns: Column<Instrument>[] = useMemo(() => columnsBuilder(
     {
@@ -42,7 +42,7 @@ export default function InstrumentView() {
   return (
     <div>
       <h2>Instruments</h2>
-      <button onClick={fetchInstruments}>Reload Instruments</button>
+      <button onClick={reload}>Reload Instruments</button>
       <DataTable
         title="Instrument List"
         data={instruments}
--- src/hooks/useTickerFetch.ts
+++ src/hooks/useTickerFetch.ts
@@ -7,23 +7,23 @@ import ApiClient from "@/services/api/api";
 import { BASE_URL } from "@/services/ticker/constants";
 import { Instrument, InstrumentResponse } from "@/models/ticker";
 
-const today = new Date();
-const defaultStartTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 30, 0, 0);
-const defaultSimulator = new StraddleDataSimulator(defaultStartTime);
+// const today = new Date();
+// const defaultStartTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9, 30, 0, 0);
+// const defaultSimulator = new StraddleDataSimulator(defaultStartTime);
 
-export function useTickerFetch() {
-  const dispatch = useAppDispatch();
-  const data = useAppSelector(selectTickerData);
-  const lastTimestamp = data.length > 0 ? data[data.length - 1].timestamp : null;
-  const fetcher = useCallback(async () => {
-    const newData = await defaultSimulator.fetchDeltaData(lastTimestamp);
-    dispatch(addSnapshots(newData));
-  }, [lastTimestamp]);
-  useEffect(() => {
-    fetcher();
-  }, [])
-  return fetcher;
-}
+// export function useTickerFetch() {
+//   const dispatch = useAppDispatch();
+//   const data = useAppSelector(selectTickerData);
+//   const lastTimestamp = data.length > 0 ? data[data.length - 1].timestamp : null;
+//   const fetcher = useCallback(async () => {
+//     const newData = await defaultSimulator.fetchDeltaData(lastTimestamp);
+//     dispatch(addSnapshots(newData));
+//   }, [lastTimestamp]);
+//   useEffect(() => {
+//     fetcher();
+//   }, [])
+//   return fetcher;
+// }
 
 const tickerClient = new TickerClient(new ApiClient({
   baseURL: BASE_URL,
