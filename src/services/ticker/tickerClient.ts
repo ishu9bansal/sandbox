3f98fa4ba447e82515c4675599caab835257e351
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-14 13:00:03 +0530

    add ticker client

---
+++ src/services/ticker/tickerClient.ts
@@ -0,0 +1,51 @@
+import ApiClient from "../api/api";
+
+export class TickerClient {
+  constructor(private client: ApiClient) {}
+  async getInstruments() {
+    const uri = `/ticker/instruments`;
+    try {
+      const response = await this.client.get(uri);
+      return response;
+    } catch (error) {
+      console.error(error); // NOTE: do not expose internal error details to user
+      throw Error("Error while fetching instruments");  // user facing message
+    }
+  }
+  async getQuote(underlying: string) {
+    const queryParams = new URLSearchParams({ underlying });
+    const queryString = queryParams.toString();
+    const uri = `/ticker/quote/?${queryString}`;
+    try {
+      const response = await this.client.get(uri);
+      return response;
+    } catch (error) {
+      console.error(error); // NOTE: do not expose internal error details to user
+      throw Error("Error while fetching quote");  // user facing message
+    }
+  }
+  async getHistory(underlying: string, from_time: Date) {
+    const from = this.formatDate(from_time); 
+    const queryParams = new URLSearchParams({ underlying, from });
+    const queryString = queryParams.toString();
+    const uri = `/ticker/history/?${queryString}`;
+    try {
+      const response = await this.client.get(uri);
+      return response;
+    } catch (error) {
+      console.error(error); // NOTE: do not expose internal error details to user
+      throw Error("Error while fetching history");  // user facing message
+    }
+  }
+  private formatDate(date: Date) {
+    return date.toLocaleString('sv-SE', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false,
+    }).replace(' ', 'T').replace(',', '');
+  }
+};
