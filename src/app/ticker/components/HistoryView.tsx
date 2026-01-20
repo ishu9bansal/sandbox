f71fe0069d2c725e8efce2c8295dc9cbe8a1a171
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-20 23:30:46 +0530

    add select component for underlying

--- src/app/ticker/components/HistoryView.tsx
+++ src/app/ticker/components/HistoryView.tsx
@@ -4,6 +4,7 @@ import Card from '@/components/compositions/card';
 import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { DatePicker } from '@/components/compositions/date-picker';
+import { SelectInput } from '@/components/compositions/select-input';
 
 
 const MARKET_OPEN_TIME = '09:15:00';
@@ -14,29 +15,35 @@ export default function HistoryView() {
   const [date, setDate] = useState(TODAY);
   const [startTime, setStartTime] = useState(MARKET_OPEN_TIME);
   const [endTime, setEndTime] = useState(MARKET_CLOSE_TIME);
+  const [underlying, setUnderlying] = useState<'NIFTY' | 'SENSEX'>('NIFTY');
   
   return (
     <div>
       <div className="flex items-center mb-4 space-x-4">
         <DatePicker date={date} onDateChange={(d) => setDate(d || TODAY)} />
         <Input
-            type='time'
-            step={1}
-            value={startTime}
-            onChange={(e) => setStartTime(e.target.value)}
-            className="w-auto"
-          />
-          <span className="text-black/60">-</span>
-          <Input
-            type='time'
-            step={1}
-            value={endTime}
-            onChange={(e) => setEndTime(e.target.value)}
-            className="w-auto"
-          />
+          type='time'
+          step={1}
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+          className="w-auto"
+        />
+        <span className="text-black/60">-</span>
+        <Input
+          type='time'
+          step={1}
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+          className="w-auto"
+        />
+        <SelectInput
+          value={underlying}
+          onChange={(value) => setUnderlying(value as 'NIFTY' | 'SENSEX')}
+          options={['NIFTY', 'SENSEX']}
+        />
       </div>
-      <Card title="User Details" collapsible>
-        <JsonView data={{ date, startTime, endTime }} />
+      <Card title="Selected Details" collapsible>
+        <JsonView data={{ date, startTime, endTime, underlying }} />
       </Card>
     </div>
   );
