37ea5f9315ca45bfcfd6676358e83a069820fab9
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-20 23:24:32 +0530

    history view controls

--- src/app/ticker/components/HistoryView.tsx
+++ src/app/ticker/components/HistoryView.tsx
@@ -1,18 +1,42 @@
 
-import { Button } from '@/components/ui/button';
 import JsonView from '@/components/JsonView';
 import Card from '@/components/compositions/card';
-import { useTickerUser } from '@/hooks/useTickerFetch';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { DatePicker } from '@/components/compositions/date-picker';
+
+
+const MARKET_OPEN_TIME = '09:15:00';
+const MARKET_CLOSE_TIME = '15:30:00';
+const TODAY = new Date();
 
 export default function HistoryView() {
-  const { reload, user } = useTickerUser();
+  const [date, setDate] = useState(TODAY);
+  const [startTime, setStartTime] = useState(MARKET_OPEN_TIME);
+  const [endTime, setEndTime] = useState(MARKET_CLOSE_TIME);
+  
   return (
     <div>
-      <div className="flex justify-end mb-4">
-        <Button onClick={reload}>Reload User</Button>
+      <div className="flex items-center mb-4 space-x-4">
+        <DatePicker date={date} onDateChange={(d) => setDate(d || TODAY)} />
+        <Input
+            type='time'
+            step={1}
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+            className="w-auto"
+          />
+          <span className="text-black/60">-</span>
+          <Input
+            type='time'
+            step={1}
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+            className="w-auto"
+          />
       </div>
       <Card title="User Details" collapsible>
-        <JsonView data={user} />
+        <JsonView data={{ date, startTime, endTime }} />
       </Card>
     </div>
   );
