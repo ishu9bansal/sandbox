41e5fa253e8ef9acf07ff82453e5ac471bc881b7
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-24 22:21:34 +0530

    add edit entry view

---
+++ src/app/periodontics/[id]/entry/[eid]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import EditLayout from "@/app/periodontics/components/EditLayout";
+import PerioInput from "@/app/periodontics/components/input/PerioInput";
+import QuickLabels from "@/components/compositions/quick-labels";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ParamEntry } from "@/models/perio";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { selectPerioRecordById, updatePerioRecord } from "@/store/slices/perioSlice";
+import { generateParamEntryId, newMeasure } from "@/utils/perio";
+import { useParams, useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+
+export default function PerioRecordEntryEditPage() {
+  const { id: record_id, eid: entry_id } = useParams();
+  const router = useRouter();
+  const recordMaybe = useAppSelector(selectPerioRecordById(record_id));
+  const record = recordMaybe!;  // already handled non-existence in layout.tsx
+  const dispatch = useAppDispatch();
+  const onSubmit = useCallback((entry: ParamEntry) => {
+    const updatedParamEntries = [...record.paramEntries];
+    const existingIndex = updatedParamEntries.findIndex(e => e.id === entry.id);
+    if (existingIndex >= 0) {
+      updatedParamEntries[existingIndex] = entry;
+    } else {
+      updatedParamEntries.push(entry);
+    }
+    dispatch(updatePerioRecord({
+      ...record,
+      paramEntries: updatedParamEntries,
+    }));
+  }, [dispatch, record]);
+  const recordEntry = record.paramEntries.find(e => e.id === entry_id);
+  const [label, setLabel] = useState(recordEntry ? recordEntry.label : '');
+  const [input, setInput] = useState(recordEntry ? recordEntry.entry : newMeasure());
+  const viewTitle = recordEntry ? "Edit Entry" : "Add Entry";
+  const handleSubmit = useCallback(() => {
+    onSubmit({
+      id: recordEntry ? recordEntry.id : generateParamEntryId(),
+      label,
+      entry: input,
+    });
+    router.push(`/periodontics/${record_id}`);
+  }, [label, input, onSubmit, router, record_id]);
+  const labels = ['PPD', 'LGM', 'Bleeding on Probing', 'Mobility', 'Furcation Involvement', 'Suppuration', 'Gingival Recession'];
+  return (
+    <div className="max-w-3xl mx-auto">
+      <EditLayout
+        title={viewTitle}
+        onSubmit={handleSubmit}
+        onCancel={router.back}
+        nextLabel={'Submit'}
+      >
+        <Label className="mb-2">Parameter Name</Label>
+        <Input
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+          placeholder="Enter parameter name"
+        />
+        <QuickLabels labels={labels} onSelect={setLabel} />
+        <PerioInput teeth={record.teeth} data={input} onUpdate={setInput} />
+      </EditLayout>
+    </div>
+  );
+}
