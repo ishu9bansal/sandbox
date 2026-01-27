276cc68b277b684e23a362a0ddf6860232f12e71
Ishu Bansal <ishubansal1400@gmail.com>
2026-01-27 20:28:16 +0530

    user token input from user

--- src/app/ticker/components/UserView.tsx
+++ src/app/ticker/components/UserView.tsx
@@ -1,19 +1,32 @@
 
 import { Button } from '@/components/ui/button';
 import JsonView from '@/components/JsonView';
-import Card from '@/components/compositions/card';
 import { useTickerUser } from '@/hooks/useTickerFetch';
+import ActionCard from '@/components/compositions/action-card';
+import { Input } from '@/components/ui/input';
+import { useCallback, useState } from 'react';
+import { toast } from 'sonner';
 
 export default function UserView() {
   const { reload, user } = useTickerUser();
+  const [token, setToken] = useState('');
+  const updateToken = useCallback(() => {
+    toast.success(`User token updated (not really, this is a placeholder): ${token}`);
+  }, [token]);
   return (
-    <div>
-      <div className="flex justify-end mb-4">
-        <Button onClick={reload}>Reload User</Button>
-      </div>
-      <Card title="User Details" collapsible>
+    <div className="space-y-6">
+      <ActionCard title="User Token" actionChildren={<Button onClick={updateToken}>Update Token</Button>} >
+        <Input
+          type="text"
+          value={token}
+          onChange={(e) => setToken(e.target.value)}
+          placeholder="Enter new user token"
+        />
+      </ActionCard>
+      <ActionCard title="User Details" actionChildren={<Button onClick={reload}>Reload User</Button>}>
         <JsonView data={user} />
-      </Card>
+      </ActionCard>
     </div>
   );
 }
+
